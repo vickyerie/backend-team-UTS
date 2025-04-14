0664cc8b00bc84417ca64c40ef24ae620c3fe420
ghost <lyviareva@gmail.com>
2025-04-14 21:22:05 +0700

    Menambahkan fitur Update

--- api/route.js
+++ api/route.js
@@ -9,49 +9,60 @@ module.exports = (app) => {
   routes.get('/classes', controller.getAllClasses);
   routes.get('/classes/:index', controller.getClassByIndex);
   routes.post('/classes', controller.createClass);
+  routes.put('/classes/:index', controller.updateClass);
   
   // Races
   routes.get('/races', controller.getAllRaces);
   routes.get('/races/:index', controller.getRaceByIndex);
   routes.post('/races', controller.createRace);
+  routes.put('/races/:index', controller.updateRace);
 
   // Spells
   routes.get('/spells', controller.getAllSpells);
   routes.get('/spells/:index', controller.getSpellByIndex);
   routes.post('/spells', controller.createSpell);
+  routes.put('/spells/:index', controller.updateSpell);
 
   // Monsters
   routes.get('/monsters', controller.getAllMonsters);
   routes.get('/monsters/:index', controller.getMonsterByIndex);
   routes.post('/monsters', controller.createMonster);
+  routes.put('/monsters/:index', controller.updateMonster);
 
   // Equipment
   routes.get('/equipment', controller.getAllEquipment);
   routes.get('/equipment/:index', controller.getEquipmentByIndex);
   routes.post('/equipment', controller.createEquipment);
+  routes.put('/equipment/:index', controller.updateEquipment);
 
   // Ability Scores
   routes.get('/ability-scores', controller.getAllAbilityScores);
   routes.get('/ability-scores/:index', controller.getAbilityScoreByIndex);
   routes.post('/ability-scores', controller.createAbilityScore);
+  routes.put('/ability-scores/:index', controller.updateAbilityScore);
 
   // Proficiencies
   routes.get('/proficiencies', controller.getAllProficiencies);
   routes.get('/proficiencies/:index', controller.getProficiencyByIndex);
   routes.post('/proficiencies', controller.createProficiency);
+  routes.put('/proficiencies/:index', controller.updateProficiency);
 
   // Skills
   routes.get('/skills', controller.getAllSkills);
   routes.get('/skills/:index', controller.getSkillByIndex);
   routes.post('/skills', controller.createSkill);
+  routes.put('/skills/:index', controller.updateSkill);
 
   // Features
   routes.get('/features', controller.getAllFeatures);
   routes.get('/features/:index', controller.getFeatureByIndex);
   routes.post('/features', controller.createFeature);
+  routes.put('/features/:index', controller.updateFeature);
 
   // Alignments
   routes.get('/alignments', controller.getAllAlignments);
   routes.get('/alignments/:index', controller.getAlignmentByIndex);
   routes.post('/alignments', controller.createAlignment);
+  routes.put('/alignments/:index', controller.updateAlignment);
+
 };
--- api/service.js
+++ api/service.js
@@ -14,61 +14,71 @@ const {
   const classService = {
     getAll: () => classRepo.getAll(),
     getByIndex: (index) => classRepo.getByIndex(index),
-    create: (data) => ClassRepo.create(data),
+    create: (data) => classRepo.create(data),
+    update: (index, data) => classRepo.update(index, data),
   };
   
   const raceService = {
     getAll: () => raceRepo.getAll(),
     getByIndex: (index) => raceRepo.getByIndex(index),
     create: (data) => raceRepo.create(data),
+    update: (index, data) => raceRepo.update(index, data),
   };
   
   const spellService = {
     getAll: () => spellRepo.getAll(),
     getByIndex: (index) => spellRepo.getByIndex(index),
     create: (data) => spellRepo.create(data),
+    update: (index, data) => spellRepo.update(index, data),
   };
   
   const monsterService = {
     getAll: () => monsterRepo.getAll(),
     getByIndex: (index) => monsterRepo.getByIndex(index),
     create: (data) => monsterRepo.create(data),
+    update: (index, data) => monsterRepo.update(index, data),
   };
   
   const equipmentService = {
     getAll: () => equipmentRepo.getAll(),
     getByIndex: (index) => equipmentRepo.getByIndex(index),
     create: (data) => equipmentRepo.create(data),
+    update: (index, data) => equipmentRepo.update(index, data),
   };
   
   const abilityScoresService = {
     getAll: () => abilityScoresRepo.getAll(),
     getByIndex: (index) => abilityScoresRepo.getByIndex(index),
-    create: (data) => abilityScoresRepo.create(data)
+    create: (data) => abilityScoresRepo.create(data),
+    update: (index, data) => abilityScoresRepo.update(index, data),
   };
   
   const proficienciesService = {
     getAll: () => proficienciesRepo.getAll(),
     getByIndex: (index) => proficienciesRepo.getByIndex(index),
     create: (data) => proficienciesRepo.create(data),
+    update: (index, data) => proficienciesRepo.update(index, data),
   };
   
   const skillsService = {
     getAll: () => skillsRepo.getAll(),
     getByIndex: (index) => skillsRepo.getByIndex(index),
     create: (data) => skillsRepo.create(data),
+    update: (index, data) => skillsRepo.update(index, data),
   };
   
   const featuresService = {
     getAll: () => featuresRepo.getAll(),
     getByIndex: (index) => featuresRepo.getByIndex(index),
     create: (data) => featuresRepo.create(data),
+    update: (index, data) => featuresRepo.update(index, data),
   };
   
   const alignmentsService = {
     getAll: () => alignmentsRepo.getAll(),
     getByIndex: (index) => alignmentsRepo.getByIndex(index),
     create: (data) => alignmentsRepo.create(data),
+    update: (index, data) => alignmentsRepo.update(index, data),
   };
   
   module.exports = {
