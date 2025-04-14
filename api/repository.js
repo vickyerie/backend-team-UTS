df9de50ad339ca36af2682b41cb32694b7d249bd
Patricia <patricia.nia27@gmail.com>
2025-04-14 20:45:38 +0700

    menambahkan fitur create

--- api/repository.js
+++ api/repository.js
@@ -14,51 +14,61 @@ const {
   const classRepo = {
     getAll: () => Classes.find({}),
     getByIndex: (index) => Classes.findOne({ index }),
+    create: (data) => Classes.create(data),
   };
   
   const raceRepo = {
     getAll: () => Races.find({}),
     getByIndex: (index) => Races.findOne({ index }),
+    create: (index, data) => Races.findOneAndUpdate({ index }, data, { new : true})
   };
   
   const spellRepo = {
     getAll: () => Spells.find({}),
     getByIndex: (index) => Spells.findOne({ index }),
+    create: (data) => Spells.create(data),
   };
   
   const monsterRepo = {
     getAll: () => Monsters.find({}),
     getByIndex: (index) => Monsters.findOne({ index }),
+    create: (data) => Monsters.create(data),
   };
   
   const equipmentRepo = {
     getAll: () => Equipment.find({}),
     getByIndex: (index) => Equipment.findOne({ index }),
+    create: (data) => Equipment.create(data),
   };
   
   const abilityScoresRepo = {
     getAll: () => AbilityScores.find({}),
     getByIndex: (index) => AbilityScores.findOne({ index }),
+    create: (data) => Monsters.create(data)
   };
   
   const proficienciesRepo = {
     getAll: () => Proficiencies.find({}),
     getByIndex: (index) => Proficiencies.findOne({ index }),
+    create: (data) => Proficiencies.create(data),
   };
   
   const skillsRepo = {
     getAll: () => Skills.find({}),
     getByIndex: (index) => Skills.findOne({ index }),
+    create: (data) => Skills.create(data),
   };
   
   const featuresRepo = {
     getAll: () => Features.find({}),
     getByIndex: (index) => Features.findOne({ index }),
+    create: (data) => Features.create(data),
   };
   
   const alignmentsRepo = {
     getAll: () => Alignments.find({}),
     getByIndex: (index) => Alignments.findOne({ index }),
+    create: (data) => Alignments.create(data),
   };
   
   module.exports = {
--- api/service.js
+++ api/service.js
@@ -14,51 +14,61 @@ const {
   const classService = {
     getAll: () => classRepo.getAll(),
     getByIndex: (index) => classRepo.getByIndex(index),
+    create: (data) => ClassRepo.create(data),
   };
   
   const raceService = {
     getAll: () => raceRepo.getAll(),
     getByIndex: (index) => raceRepo.getByIndex(index),
+    create: (data) => raceRepo.create(data),
   };
   
   const spellService = {
     getAll: () => spellRepo.getAll(),
     getByIndex: (index) => spellRepo.getByIndex(index),
+    create: (data) => spellRepo.create(data),
   };
   
   const monsterService = {
     getAll: () => monsterRepo.getAll(),
     getByIndex: (index) => monsterRepo.getByIndex(index),
+    create: (data) => monsterRepo.create(data),
   };
   
   const equipmentService = {
     getAll: () => equipmentRepo.getAll(),
     getByIndex: (index) => equipmentRepo.getByIndex(index),
+    create: (data) => equipmentRepo.create(data),
   };
   
   const abilityScoresService = {
     getAll: () => abilityScoresRepo.getAll(),
     getByIndex: (index) => abilityScoresRepo.getByIndex(index),
+    create: (data) => abilityScoresRepo.create(data)
   };
   
   const proficienciesService = {
     getAll: () => proficienciesRepo.getAll(),
     getByIndex: (index) => proficienciesRepo.getByIndex(index),
+    create: (data) => proficienciesRepo.create(data),
   };
   
   const skillsService = {
     getAll: () => skillsRepo.getAll(),
     getByIndex: (index) => skillsRepo.getByIndex(index),
+    create: (data) => skillsRepo.create(data),
   };
   
   const featuresService = {
     getAll: () => featuresRepo.getAll(),
     getByIndex: (index) => featuresRepo.getByIndex(index),
+    create: (data) => featuresRepo.create(data),
   };
   
   const alignmentsService = {
     getAll: () => alignmentsRepo.getAll(),
     getByIndex: (index) => alignmentsRepo.getByIndex(index),
+    create: (data) => alignmentsRepo.create(data),
   };
   
   module.exports = {
